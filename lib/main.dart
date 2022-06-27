770ff3383e56edd0a5f89f83141fb9a9002d6b5d
RaniaAhmedm <raniaahmedm12@gmail.com>
2022-06-27 08:34:12 +0200

    just initialize app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app/Home/home-screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
